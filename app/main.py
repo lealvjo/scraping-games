2ff4e24e02b76b4a489631305e728dee42ead4de
lealvjo <leonardo.jose.barros@hotmail.com>
2021-04-06 19:20:26 -0300

    heroku server

--- app/main.py
+++ app/main.py
@@ -5,42 +5,45 @@ This method is called when generated the packet by setup.py, or when you need to
 from flask import Flask, request
 from app.src.orchestrator.initializer import Initializer
 
-route_initializer = Initializer()
-app = Flask(__name__)
 
+def create_app():
 
-@app.route(route_initializer.endpoint['home'], methods=['GET'])
-def route_home():
-    return route_initializer.home()
+    route_initializer = Initializer()
+    app = Flask(__name__)
 
+    @app.route('/')
+    def index():
+        return route_initializer.endpoint
 
-@app.route(route_initializer.endpoint['search_for_id'], methods=['GET'])
-def route_games_per_id(id):
-    return route_initializer.games_per_id(id)
+    @app.route(route_initializer.endpoint['home'], methods=['GET'])
+    def route_home():
+        return route_initializer.home()
 
+    @app.route(route_initializer.endpoint['search_for_id'], methods=['GET'])
+    def route_games_per_id(id):
+        return route_initializer.games_per_id(id)
 
-@app.route(route_initializer.endpoint['search_for_index_page'], methods=['GET'])
-def rout_games_per_page(page):
-    return route_initializer.games_per_page(page)
+    @app.route(route_initializer.endpoint['search_for_index_page'], methods=['GET'])
+    def rout_games_per_page(page):
+        return route_initializer.games_per_page(page)
 
+    @app.route(route_initializer.endpoint['home'], methods=['POST'])
+    def route_save_game():
+        return route_initializer.save_game(request.get_json())
 
-@app.route(route_initializer.endpoint['home'], methods=['POST'])
-def route_save_game():
-    return route_initializer.save_game(request.get_json())
+    @app.route(route_initializer.endpoint['search_for_id'], methods=['PUT'])
+    def route_change_price(id):
+        return route_initializer.change_price(id, request.get_json())
 
+    @app.route(route_initializer.endpoint['search_for_id'], methods=['DELETE'])
+    def route_delete_game(id):
+        return route_initializer.delete_game(id)
 
-@app.route(route_initializer.endpoint['search_for_id'], methods=['PUT'])
-def route_change_price(id):
-    return route_initializer.change_price(id, request.get_json())
-
-
-@app.route(route_initializer.endpoint['search_for_id'], methods=['DELETE'])
-def route_delete_game(id):
-    return route_initializer.delete_game(id)
+    return app
 
 
 def main():
-    app.run(debug=True)
+    create_app().run()
 
 
 if __name__ == '__main__':
